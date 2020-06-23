b02fb0ddc61c80187f9efc4626502d90a74e9721
nishi <nishiwest.k@gmail.com>
2020-06-23 16:34:10 +0900

    [start]Start "TensorClassifier".

--- tensor_decomposition/tensor_decomposition.py
+++ tensor_decomposition/tensor_decomposition.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
 from sklearn.metrics import mean_squared_error
 from sklearn.decomposition import TruncatedSVD
 import tensorly as tl
@@ -9,6 +9,49 @@ from tensorly.decomposition import tucker
 
 
 class TuckerRecommendation(BaseEstimator, RegressorMixin):
+    
+    def __init__(self, shape=[None, None, None], rank=[None, None, None], missing_val='mean',
+                 n_iter_max=100, tol=0.0001, random_state=0, verbose=False):
+        self.shape = shape
+        self.rank = rank
+        self.missing_val = missing_val   
+        self.n_iter_max = n_iter_max
+        self.tol = tol
+        self.random_state = random_state
+        self.verbose = verbose
+
+
+    def fit(self, X, y):
+        X = X.astype(int)
+        tensor = np.full(self.shape, np.nan)
+        tensor[tuple(X.T)] = y
+
+        if self.missing_val == 'mean':
+            tensor[np.isnan(tensor)] = np.nanmean(tensor)
+        else:
+            tensor[np.isnan(tensor)] = self.missing_val
+
+        self.core, self.factors = tucker(tensor, rank=self.rank, n_iter_max=self.n_iter_max,
+                                         tol=self.tol, random_state=self.random_state,
+                                         verbose=self.verbose)
+
+        self.tucker_tensor = tl.tucker_to_tensor(self.core, self.factors)
+        return self
+
+
+    def predict(self, test_X):
+        tucker_tensor = self.tucker_tensor
+        return tucker_tensor[tuple(test_X.T)]
+    
+            
+    def score(self, test_X, test_y):
+        test_X = test_X.astype(int)
+        prediction = self.predict(test_X)
+        mse = mean_squared_error(test_y, prediction)
+        return -np.sqrt(mse)
+
+
+class TuckerClassifier(BaseEstimator, ClassifierMixin):
     
     def __init__(self, shape=[None, None, None], rank=[None, None, None], missing_val='mean',
                  n_iter_max=100, tol=0.0001, random_state=0, verbose=False):
